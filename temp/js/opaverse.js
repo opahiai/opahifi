fbab58f3ecf0f32306ba899dd8cccba5701752f8
EliranT <eliranoo@gmail.com>
2026-07-11 05:42:35 -0400

    Remove temporary body text file from the repository

--- temp/js/opaverse.js
+++
@@ -1,5 +0,0 @@
-import { OpaverseController } from './opaverse/OpaverseController.js';
-
-document.addEventListener('DOMContentLoaded', () => {
-    new OpaverseController(window.opaHifiDatabase);
-});
