1b41f89336fb5f48c910a7e4980de57f2a3aace2
EliranT <eleaptech@gmail.com>
2026-07-21 19:49:11 -0400

    Register Do The Panicarena

--- src/opaverses/opaverse.registry.js
+++ src/opaverses/opaverse.registry.js
@@ -6,6 +6,7 @@ import { ohSongModule as ohBelieveTheTruthFairyModule } from "./believe-the-trut
 import { ohSongModule as ohOldLoveStoryModule } from "./old-love-story/index.js";
 import { ohSongModule as ohGlittaaPhoenixModule } from "./glittaa-phoenix/index.js";
 import { ohSongModule as ohNotYourBotBeepSleepModule } from "./not-your-bot-beep-sleep/index.js";
+import { ohSongModule as ohDoThePanicarenaModule } from "./do-the-panicarena/index.js";
 import { ohSongModule as ohWellwolfHowlLehluyaModule } from "./wellwolf-howl-lehluya/index.js";
 import { ohSongModule as ohOpaPaPaPartyModule } from "./opa-pa-pa-party/index.js";
 
@@ -18,6 +19,7 @@ export const OH_OPAVERSE_MODULES = Object.freeze([
   ohOldLoveStoryModule,
   ohGlittaaPhoenixModule,
   ohNotYourBotBeepSleepModule,
+  ohDoThePanicarenaModule,
   ohWellwolfHowlLehluyaModule,
   ohOpaPaPaPartyModule
 ]);
