cea25319f7fb4b812d3f38a516c7b9c74a7c07ed
EliranT <eleaptech@gmail.com>
2026-09-13 16:05:53 -0500

    audionutirtn

--- src/opaverses/catalog.config.js
+++ src/opaverses/catalog.config.js
@@ -1,6 +1,6 @@
 export const OH_FEATURED_SONG = Object.freeze({
   id: "do-the-panicarena",
-  label: "Latest release"
+  label: "SPOTLIGHT"
 });
 
 export const OH_SONG_SETTINGS = Object.freeze({
@@ -15,5 +15,5 @@ export const OH_SONG_SETTINGS = Object.freeze({
   "do-the-panicarena": Object.freeze({ enabled: true }),
   "wellwolf-howl-lehluya": Object.freeze({ enabled: true }),
   "opa-pa-pa-party": Object.freeze({ enabled: true }),
-  "optimisms-cute": Object.freeze({ enabled: false })
+  "optimisms-cute": Object.freeze({ enabled: true })
 });
